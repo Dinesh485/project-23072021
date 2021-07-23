f5a044c328af983cba237ee49b9385b67b1a6b33
Dinesh485 <ghantasaladinesh@gmail.com>
2021-07-23 16:10:21 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode:'jit',
+  purge: ['./index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      exo : ["'Exo 2'", 'sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
